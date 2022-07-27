8d74f8b927aa4fde764b5a452a93c5af865cc695
Constanza Castillo <cony.castillo@gmail.com>
2022-07-26 22:05:33 -0400

    guardar cambios realizados en esta aplicación

---
+++ app/src/main/java/com/example/mvvmexample/ui/viewModel/QuoteViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.mvvmexample.ui.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mvvmexample.data.model.QuoteModel
+import com.example.mvvmexample.data.model.QuoteProvider
+
+class QuoteViewModel : ViewModel() {
+    val quoteModel = MutableLiveData<QuoteModel>()
+
+    fun randomQuote(){
+        val currentQuote = QuoteProvider.random()
+        quoteModel.postValue(currentQuote)
+    }
+}
